7aa9df725212d421ce913bfc5217fdd4f07cbea9
Raúl <raulacostadeveloper@outlook.com>
2023-10-16 11:22:21 -0600

    modal al intentar entrar

--- src/app/Elements/Login.tsx
+++ src/app/Elements/Login.tsx
@@ -8,52 +8,71 @@ const adminUserPassword = '1234';
 export const Login = ({ handleLogin }: Props): JSX.Element => {
     const [ inputUsuarioValue, setInputUsuarioValue ] = useState('');
     const [ inputPasswordValue, setInputPasswordValue ] = useState('');
+    const [ showModalInfo, setShowModalInfo ] = useState(false);
     const handleTryLogin = () => {
         setInputUsuarioValue('');
         setInputPasswordValue('');
         if (inputUsuarioValue === adminUserName && inputPasswordValue === adminUserPassword) {
             handleLogin(true);
         } else {
-            console.log('Usuario o contraseña incorrectos');
+            setShowModalInfo(true);
         }
     }
     const handleLogEmpleado = () => {
         handleLogin(false);
     }
     return (
-        <div className="containerModal">
-            <div className="innerModal">
-                <div className='titulo'> 
-                    <h2>Login</h2>
-                    <p>Login del usuario administrador</p>
-                 </div>
-                <div className='input'>
-                    <input 
-                        type="text" 
-                        placeholder='Usuario' 
-                        className='input-1'
-                        autoComplete="off"
-                        value={ inputUsuarioValue }
-                        onChange={ (event) => setInputUsuarioValue(event.target.value) }/>
-                </div>
-                <div className='input'>
-                    <input 
-                        type="password" 
-                        placeholder='Contraseña' 
-                        className='input-1'
-                        autoComplete="off"
-                        value={ inputPasswordValue }
-                        onChange={ (event) => setInputPasswordValue(event.target.value) }/>
-                </div>
-                <div className='boton'>
-                    <button className='boton-1' onClick={ () => handleTryLogin() }>Ingresar</button>
-                </div>
-                    
-                <div className='boton'>
-                    <p className='adminq'>¿No eres administrador?</p>
-                    <button className='boton-1' onClick={ () => handleLogEmpleado() }>Soy empleado</button>
+        <>
+            <div className="containerModal">
+                <div className="innerModal">
+                    <div className='titulo'> 
+                        <h2>Login</h2>
+                        <p>Login del usuario administrador</p>
+                    </div>
+                    <div className='input'>
+                        <input 
+                            type="text" 
+                            placeholder='Usuario' 
+                            className='input-1'
+                            autoComplete="off"
+                            value={ inputUsuarioValue }
+                            onChange={ (event) => setInputUsuarioValue(event.target.value) }/>
+                    </div>
+                    <div className='input'>
+                        <input 
+                            type="password" 
+                            placeholder='Contraseña' 
+                            className='input-1'
+                            autoComplete="off"
+                            value={ inputPasswordValue }
+                            onChange={ (event) => setInputPasswordValue(event.target.value) }/>
+                    </div>
+                    <div className='boton'>
+                        <button className='boton-1' onClick={ () => handleTryLogin() }>Ingresar</button>
+                    </div>
+                        
+                    <div className='boton'>
+                        <p className='adminq'>¿No eres administrador?</p>
+                        <button className='boton-1' onClick={ () => handleLogEmpleado() }>Soy empleado</button>
+                    </div>
                 </div>
             </div>
-        </div>
+            { showModalInfo && 
+                <div className='containerModal'>
+                    <div className='innerModal'>
+                        <div className='titulo'>
+                            <h3>No se encontró el usuario</h3>
+                        </div>
+                        <div className='texto'>
+                            Para pruebas use admin 1234
+                        </div>
+
+                        <div className='boton'>
+                            <button className='boton-1' onClick={ ()=> setShowModalInfo(false) }>Aceptar</button>
+                        </div>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
